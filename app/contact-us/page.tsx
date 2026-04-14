01d15c992fb38c26368b926dacbaa74bbf2153d9
shafiqahmad81 <shafiqahmad081@gmail.com>
2026-04-14 20:19:55 +0600

    complete home page and create contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -4,12 +4,8 @@ import LeadershipSection from "@/components/leadership/leadership";
 export default function AboutUs() {
     return (
         <>
-            <section className="py-5 sm:py-10">
-                <div className="container">
-                    <LeadershipSection />
-                    <FAQSection />
-                </div>
-            </section>
+            <LeadershipSection />
+            <FAQSection />
         </>
     );
 }
