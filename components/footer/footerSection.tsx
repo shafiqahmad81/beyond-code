5064df7eee49e11db306f0c0ba5d51d95f9caf5d
shafiqahmad81 <shafiqahmad081@gmail.com>
2026-04-17 20:30:42 +0600

    update button

--- components/footer/footerSection.tsx
+++ components/footer/footerSection.tsx
@@ -53,35 +53,35 @@ export default function Footer() {
                                 <span>Book for Consultation</span>
 
                                 
-                                    <svg
-        className="arrow-icon-con"
-        xmlns="http://www.w3.org/2000/svg"
-        width="33"
-        height="33"
-        viewBox="0 0 32 32"
-        fill="none"
-      >
-        <g
-          fill="none"
-          stroke="red"
-          strokeWidth="1.5"
-          strokeLinejoin="round"
-          strokeMiterlimit="10"
-        >
-          {/* Circle */}
-          <circle className="arrow-icon--circle-con" cx="16" cy="16" r="15.12" />
+                                <svg
+                                    className="arrow-icon-con"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="33"
+                                    height="33"
+                                    viewBox="0 0 32 32"
+                                    fill="none"
+                                >
+                                    <g
+                                        fill="none"
+                                        stroke="black"
+                                        strokeWidth="1.5"
+                                        strokeLinejoin="round"
+                                        strokeMiterlimit="10"
+                                    >
+                                        {/* Circle */}
+                                        <circle className="arrow-icon--circle-con" cx="16" cy="16" r="15.12" />
 
-          {/* Centered icon */}
-          <g transform="translate(9,8) scale(0.8)">
-            <path
-              fill="#000000"
-              stroke="#000000"
-              strokeWidth=".2"
-              d="M9 2.15a7.6 7.6 0 0 1 7.6 7.6V15a.85.85 0 0 1-.85.85H13.5a2.35 2.35 0 0 1-2.35-2.35V12a2.35 2.35 0 0 1 2.35-2.35h1.398a5.899 5.899 0 0 0-11.795 0H4.5A2.35 2.35 0 0 1 6.85 12v1.5a2.35 2.35 0 0 1-2.35 2.35H2.25A.85.85 0 0 1 1.4 15V9.75A7.6 7.6 0 0 1 9 2.15Zm4.5 9.2a.65.65 0 0 0-.65.65v1.5a.65.65 0 0 0 .65.65h1.4v-2.8zm-10.4 2.8h1.4a.65.65 0 0 0 .65-.65V12a.65.65 0 0 0-.65-.65H3.1z"
-            />
-          </g>
-        </g>
-      </svg>
+                                        {/* Centered icon */}
+                                        <g transform="translate(9,8) scale(0.8)">
+                                            <path
+                                                fill="#000000"
+                                                stroke="#000000"
+                                                strokeWidth=".2"
+                                                d="M9 2.15a7.6 7.6 0 0 1 7.6 7.6V15a.85.85 0 0 1-.85.85H13.5a2.35 2.35 0 0 1-2.35-2.35V12a2.35 2.35 0 0 1 2.35-2.35h1.398a5.899 5.899 0 0 0-11.795 0H4.5A2.35 2.35 0 0 1 6.85 12v1.5a2.35 2.35 0 0 1-2.35 2.35H2.25A.85.85 0 0 1 1.4 15V9.75A7.6 7.6 0 0 1 9 2.15Zm4.5 9.2a.65.65 0 0 0-.65.65v1.5a.65.65 0 0 0 .65.65h1.4v-2.8zm-10.4 2.8h1.4a.65.65 0 0 0 .65-.65V12a.65.65 0 0 0-.65-.65H3.1z"
+                                            />
+                                        </g>
+                                    </g>
+                                </svg>
                             </motion.button>
                         </div>
                         <div className="w-full md:max-w-102.75 group">
