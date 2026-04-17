9b8fecf013fe9716fa5c5c7603f0ae77cb138a4d
shafiqahmad81 <shafiqahmad081@gmail.com>
2026-04-17 22:20:50 +0600

    update translate

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,143 +3,186 @@
 import Link from "next/link";
 import type { MouseEvent } from "react";
 import { Globe, Menu, X, Headphones } from "lucide-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import GradientButton from "../button/GradientButton";
 import WithoutContentButton from "../button/withoutContentButton";
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [showTranslate, setShowTranslate] = useState(false);
 
- 
-  const navItems = [
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src =
+            "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        script.async = true;
+        document.body.appendChild(script);
 
-    { label: "About", href: "/#about", target: "#about" },
-    { label: "Why Us", href: "/#why-us", target: "#why-us" },
-    { label: "Systems", href: "/#systems", target: "#systems" },
-    { label: "Services", href: "/#services", target: "#services" },
-    { label: "Technology", href: "/#technology", target: "#technology" },
-    { label: "Process", href: "/#process", target: "#process" },
-    { label: "Case Study", href: "/case-study", target: "#case-study" },
-    { label: "Contact", href: "/contact-us", target: "#contact-us" },
-  ];
+        (window as any).googleTranslateElementInit = () => {
+            new (window as any).google.translate.TranslateElement(
+                {
+                    pageLanguage: "bn",
+                    includedLanguages: "en,bn",
+                    layout:
+                        (window as any).google.translate.TranslateElement.InlineLayout.SIMPLE,
+                },
+                "google_translate_element"
+            );
+        };
+    }, []);
 
-const smoothScroll = (
-  e: MouseEvent<HTMLElement>,
-  id: string
-) => {
-  if (id.startsWith("/#")) {
-    e.preventDefault();
+    const toggleTranslate = () => {
+        setShowTranslate((prev) => !prev);
+    };
 
-    const section = document.getElementById(id.replace("/#", ""));
-    if (section) {
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  }
-};
+    const navItems = [
 
-  return (
-    <header className="w-full">
-      <div className="mx-auto max-w-368 px-4 py-4 sm:py-6">
-          <div className="rounded-full bg-[#1A1A1A] bg-blend-lighten"  >
-                  <div className="flex items-center justify-between px-6 sm:px-8 xl:px-10 gap-5 py-2 sm:py-4 backdrop-blur-[10px] rounded-full" style={{boxShadow: "inset 0px 0px 5px rgba(242, 242, 242, 0.5), inset 0px 0px 0px 0px #999999, inset 2px 2px 1px -2px #B3B3B3, inset -2px -2px 1px -2px #B3B3B3, inset 3px 3px 0px -3px rgba(0, 0, 0, 0.5)"}}>
-                  
-                  <Link href="/">
-                      <h1 className="text-2xl sm:text-[1.938rem] leading-8 font-bold">BC.</h1>
-                  </Link>
+        { label: "About", href: "/#about", target: "#about" },
+        { label: "Why Us", href: "/#why-us", target: "#why-us" },
+        { label: "Systems", href: "/#systems", target: "#systems" },
+        { label: "Services", href: "/#services", target: "#services" },
+        { label: "Technology", href: "/#technology", target: "#technology" },
+        { label: "Process", href: "/#process", target: "#process" },
+        { label: "Case Study", href: "/case-study", target: "#case-study" },
+        { label: "Contact", href: "/contact-us", target: "#contact-us" },
+    ];
 
-                  <nav className="hidden gap-5 text-sm text-white lg:flex flex-wrap">
-                      
-                      {navItems.map((item) =>
-                            item.href ? (
-                            <Link
-                                key={item.label}
-                                href={item.href}
-                                className="text-sm font-semibold transition hover:text-white/80 xl:text-base xl:leading-6"
-                            >
-                                {item.label}
-                            </Link>
-                            ) : (
+    const smoothScroll = (
+        e: MouseEvent<HTMLElement>,
+        id: string
+    ) => {
+        if (id.startsWith("/#")) {
+            e.preventDefault();
+
+            const section = document.getElementById(id.replace("/#", ""));
+            if (section) {
+                section.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                });
+            }
+        }
+    };
+
+    return (
+        <header className="w-full">
+            <div className="mx-auto max-w-368 px-4 py-4 sm:py-6">
+                <div className="rounded-full bg-[#1A1A1A] bg-blend-lighten"  >
+                    <div className="flex items-center justify-between px-6 sm:px-8 xl:px-10 gap-5 py-2 sm:py-4 backdrop-blur-[10px] rounded-full" style={{ boxShadow: "inset 0px 0px 5px rgba(242, 242, 242, 0.5), inset 0px 0px 0px 0px #999999, inset 2px 2px 1px -2px #B3B3B3, inset -2px -2px 1px -2px #B3B3B3, inset 3px 3px 0px -3px rgba(0, 0, 0, 0.5)" }}>
+
+                        <Link href="/">
+                            <h1 className="text-2xl sm:text-[1.938rem] leading-8 font-bold">BC.</h1>
+                        </Link>
+
+                        <nav className="hidden gap-5 text-sm text-white lg:flex flex-wrap">
+
+                            {navItems.map((item) =>
+                                item.href ? (
+                                    <Link
+                                        key={item.label}
+                                        href={item.href}
+                                        className="text-sm font-semibold transition hover:text-white/80 xl:text-base xl:leading-6"
+                                    >
+                                        {item.label}
+                                    </Link>
+                                ) : (
+                                    <button
+                                        key={item.label}
+                                        onClick={(e) => smoothScroll(e, item.target)}
+                                        className="cursor-pointer text-sm font-semibold transition hover:text-white/80 xl:text-base xl:leading-6"
+                                    >
+                                        {item.label}
+                                    </button>
+                                )
+                            )}
+                        </nav>
+                        <div className="flex items-center gap-4">
+                            <div className="hidden lg:flex items-center gap-2.5">
+                                <button className="text-sm xl:text-base xl:leading-6 font-semibold cursor-pointer text-white">EN</button>
+                                <div className="w-9 flex items-center justify-center relative">
+                                    <Globe onClick={toggleTranslate} className="h-4 sm:h-6 w-4 sm:w-6 text-white cursor-pointer" />
+                                    <div
+                                        id="google_translate_element"
+                                        className={`absolute top-10 right-0 bg-white text-black rounded shadow transition-all ${showTranslate
+                                            ? "opacity-100 visible"
+                                            : "opacity-0 invisible"
+                                            }`}
+                                    />
+                                </div>
+                                <button className="text-sm xl:text-base xl:leading-6 font-semibold cursor-pointer">AR</button>
+                            </div>
+
+
+                            <div className="hidden xl:block">
+                                <GradientButton
+                                    text="Request a Consultation"
+                                    icon={<Headphones className="h-4 w-4" />}
+                                />
+                            </div>
+                            <div className="hidden lg:block">
+                                <div className="xl:hidden">
+                                    <WithoutContentButton
+                                        icon={<Headphones className="h-4 w-4" />}
+                                    />
+                                </div>
+                            </div>
+
                             <button
-                                key={item.label}
-                                onClick={(e) => smoothScroll(e, item.target)}
-                                className="cursor-pointer text-sm font-semibold transition hover:text-white/80 xl:text-base xl:leading-6"
+                                className="flex lg:hidden items-center justify-center p-2 rounded-full text-white hover:bg-white/10 transition"
+                                onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
                             >
-                                {item.label}
+                                {mobileMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
                             </button>
+                        </div>
+                    </div>
+                </div>
+                {mobileMenuOpen && (
+                    <nav className="lg:hidden mt-4 flex flex-col gap-3 rounded-xl border border-white/15 bg-white/10 p-4 backdrop-blur-xl shadow-2xl">
+
+                        {navItems.map((item) =>
+                            item.href ? (
+                                <Link
+                                    key={item.label}
+                                    href={item.href}
+                                    onClick={() => setMobileMenuOpen(false)}
+                                    className="transition text-white hover:text-white/80"
+                                >
+                                    {item.label}
+                                </Link>
+                            ) : (
+                                <button
+                                    key={item.label}
+                                    onClick={(e) => smoothScroll(e, item.target)}
+                                    className="text-left transition text-white hover:text-white/80"
+                                >
+                                    {item.label}
+                                </button>
                             )
                         )}
-                  </nav>
-                  <div className="flex items-center gap-4">
-                      <div className="hidden lg:flex items-center gap-2.5">
-                          <button className="text-sm xl:text-base xl:leading-6 font-semibold cursor-pointer text-white">EN</button>
-                          <Globe className="h-4 w-4 text-white" />
-                          <button className="text-sm xl:text-base xl:leading-6 font-semibold cursor-pointer">AR</button>
-                      </div>
 
-                      
-                      <div className="hidden xl:block">
-                          <GradientButton
-                              text="Request a Consultation"
-                              icon={<Headphones className="h-4 w-4" />}
-                          />
-                      </div>
-                      <div className="hidden lg:block">
-                        <div className="xl:hidden">
-                            <WithoutContentButton
-                                icon={<Headphones className="h-4 w-4" />}
-                            />
-                        </div>
-                      </div>
+                        <GradientButton
+                            text="Request a Consultation"
+                            icon={<Headphones className="h-4 w-4" />}
+                        />
 
-                      <button
-                          className="flex lg:hidden items-center justify-center p-2 rounded-full text-white hover:bg-white/10 transition"
-                          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-                      >
-                          {mobileMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
-                      </button>
-                  </div>
-              </div>
-          </div>
-          {mobileMenuOpen && (
-              <nav className="lg:hidden mt-4 flex flex-col gap-3 rounded-xl border border-white/15 bg-white/10 p-4 backdrop-blur-xl shadow-2xl">
-                  
-                    {navItems.map((item) =>
-                        item.href ? (
-                            <Link
-                                key={item.label}
-                                href={item.href}
-                                onClick={() => setMobileMenuOpen(false)}
-                                className="transition text-white hover:text-white/80"
-                                >
-                                {item.label}
-                            </Link>
-                        ) : (
-                            <button
-                            key={item.label}
-                            onClick={(e) => smoothScroll(e, item.target)}
-                            className="text-left transition text-white hover:text-white/80"
-                            >
-                            {item.label}
-                            </button>
-                        )
-                    )}
-
-                  <GradientButton  
-                      text="Request a Consultation"
-                      icon={<Headphones className="h-4 w-4" />}
-                  />
-
-                  <div className="mt-4 flex items-center gap-3 text-sm text-white/90">
-                      <span className="font-medium">EN</span>
-                      <Globe className="h-4 w-4" />
-                      <span className="text-white/60">AR</span>
-                  </div>
-              </nav>
-          )}
-      </div>
-  </header>
-  );
+                        <div className="mt-4 flex items-center gap-3">
+                                <button className="text-sm xl:text-base xl:leading-6 font-semibold cursor-pointer text-white">EN</button>
+                                <div className="flex items-center justify-center relative">
+                                    <Globe onClick={toggleTranslate} className="h-4 sm:h-6 w-4 sm:w-6 text-white cursor-pointer" />
+                                    <div
+                                        id="google_translate_element"
+                                        className={`absolute top-10 right-0 bg-white text-black rounded shadow transition-all ${showTranslate
+                                            ? "opacity-100 visible"
+                                            : "opacity-0 invisible"
+                                            }`}
+                                    />
+                                </div>
+                                <button className="text-sm xl:text-base xl:leading-6 font-semibold cursor-pointer">AR</button>
+                            </div>
+                    </nav>
+                )}
+            </div>
+        </header>
+    );
 }
